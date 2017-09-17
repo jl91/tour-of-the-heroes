dc3489c0de0f8a83a80b93f76de1948861952156
John Lennnon de Melo Fernandes <jlfernandes91@hotmail.com>
2017-09-17 17:40:04 -0300

    first commit from ng app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    template: '<h1>{{title}}</h1>\n' +
+    '  <h2>{{hero.name}} details!</h2>\n' +
+    '  <div><label>id: </label>{{hero.id}}</div>\n' +
+    '  <div><label>name: </label>{{hero.name}}</div>'
 })
+
 export class AppComponent {
-  title = 'app';
+    title = 'Tour of heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
 }
