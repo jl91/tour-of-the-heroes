3622e55b9cd2127f7bb7d12a4ed43b10133a856c
John Lennnon de Melo Fernandes <jlfernandes91@hotmail.com>
2017-09-17 23:40:40 -0300

    adding hero collection list, without components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,33 @@
 import {Component} from '@angular/core';
 
+const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magneta'},
+    {id: 16, name: 'RubberMan'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Dr IQ'},
+    {id: 19, name: 'Magma'},
+    {id: 20, name: 'Tornado'}
+];
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    template: '<h1>{{title}}</h1>\n' +
-    '  <h2>{{hero.name}} details!</h2>\n' +
-    '  <div><label>id: </label>{{hero.id}}</div>\n' +
-    '  <div><label>name: </label>{{hero.name}}</div>'
 })
+
 
 export class AppComponent {
     title = 'Tour of heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    heroes = HEROES;
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
 
 export class Hero {
