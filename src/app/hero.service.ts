a1324b714314e870402061687aebad4022176bc3
John Lennnon de Melo Fernandes <jlfernandes91@hotmail.com>
2017-09-18 19:32:49 -0300

    adding mocked http request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,31 @@
 import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+
+    private heroesUrl = 'api/heroes';  // URL to web api
+
+    constructor(private http: Http) {
     }
 
-    getHeroesSlowly(): Promise<Hero[]> {
-        return new Promise(resolve => {
-            // Simulate server latency with 2 second delay
-            setTimeout(() => resolve(this.getHeroes()), 2000);
-        });
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
     }
+
 }
