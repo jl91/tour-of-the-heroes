3baad37059e7f725bcf67c9c1d0147e5c4da30c4
John Lennnon de Melo Fernandes <jlfernandes91@hotmail.com>
2017-09-18 16:42:09 -0300

    Added the Angular router to navigate among different components. used router link parameters to navigate to the details of the user-selected hero. shared the HeroService among multiple components. moved HTML and CSS out of the component file and into their own files. added the uppercase pipe to format data.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from "@angular/core";
-import {HEROES} from "./mock-heroes";
-import {Hero} from "./hero";
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class HeroService {
@@ -13,5 +13,10 @@ export class HeroService {
             // Simulate server latency with 2 second delay
             setTimeout(() => resolve(this.getHeroes()), 2000);
         });
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
     }
 }
